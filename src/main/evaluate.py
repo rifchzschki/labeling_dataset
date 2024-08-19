0e4c7e775bf13c74d3aa309bc82d9bfb04ca93f7
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-08-19 14:06:10 +0700

    feat: evaluate.py

---
+++ src/main/evaluate.py
@@ -0,0 +1,63 @@
+import sys
+import re
+
+def lcs(text1, text2):
+    n = len(text1)
+    m = len(text2)
+
+    prev = [0] * (m + 1)
+    cur = [0] * (m + 1)
+
+    for idx1 in range(1, n + 1):
+        for idx2 in range(1, m + 1):
+            if text1[idx1 - 1] == text2[idx2 - 1]:
+                cur[idx2] = 1 + prev[idx2 - 1]
+            else:
+                cur[idx2] = max(cur[idx2 - 1], prev[idx2])
+        prev = cur[:]
+
+    return cur[m]
+
+def precision_string(str, pattern):
+    return lcs(str, pattern) / len(pattern)
+
+def calculate_matrix(list_str, list_pattern):
+    i = 0
+    idx_found = 0
+    prec_all = 0
+    mul = 0
+
+    while i < len(list_pattern):
+        prec = 0
+        j = max(idx_found - 5, 0)
+
+        while j < len(list_str):
+            tmp_prec = precision_string(list_str[j], list_pattern[i])
+            if prec < tmp_prec:
+                print(list_pattern[i])
+                print(list_str[j])
+                prec = tmp_prec
+                idx_found = j
+            j += 1
+
+        print(list_pattern[i])
+        print(prec, "\n")
+
+        if prec <= 0.5:
+            print("Precision : 0 (Fake)")
+            return
+
+        if i != 0:
+            prec_all *= prec
+            prec_all += mul
+        else:
+            mul = prec
+            prec_all += 1
+
+        mul *= prec
+        i += 1
+
+    print("Precision", mul * len(list_pattern) / prec_all)
+
+def remove_non_alphanumeric(s):
+    return re.sub(r'\W+', '', s)
--- src/main/verification.py
+++ src/main/verification.py
@@ -1,10 +1,11 @@
 import subprocess, time, argparse
+from evaluate import calculate_matrix
 
 tmp = "../../data/tmp/"
 
-def runCalculate(engine, target, result):
+def runCalculate( target, result):
     # Menjalankan program C++ dengan kedua file sebagai argumen
-    result = subprocess.run([engine, target, result], capture_output=True, text=True)
+    result = calculate_matrix(target, result)
 
     # Menampilkan output dari program C++
     with open(tmp+"hasil.txt", 'w') as file1:
@@ -15,17 +16,16 @@ def runCalculate(engine, target, result):
 def run(engine, target, result):
         print("Verification process...")
         current_time = time.time()
-        runCalculate(engine, tmp+target, tmp+result)
+        runCalculate(tmp+target, tmp+result)
         print(f'\n\nProcessing finished in {time.time() - current_time} seconds')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "verification")
     parser.add_argument("target", type=str, help="Target file SLO (True)")
     parser.add_argument("result", type=str, help="Result from OCR (Check)")
-    parser.add_argument("engine", type=str, help="File .exe checker")
     args = parser.parse_args()
 
-    run(args.engine, args.target, args.result)
+    run(args.target, args.result)
 
 
 
